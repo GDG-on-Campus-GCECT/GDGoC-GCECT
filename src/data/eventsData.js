a37a0255fe6e538e94292e9e995ea31621600baa
Spidy394 <79023855+Spidy394@users.noreply.github.com.>
2025-10-22 17:44:13 +0530

    add event

--- src/data/eventsData.js
+++ src/data/eventsData.js
@@ -3,6 +3,7 @@
 import IntroductoryEvent from "../assets/images/events/intro.webp";
 import CloudStudyJam from "../assets/images/events/cloud.webp";
 import Hacktoberfest from "../assets/images/events/hacktoberfest.webp";
+import GeminiHackDay from "../assets/images/events/hackday.webp";
 const allEvents = [
   {
     id: 1,
@@ -19,7 +20,7 @@ const allEvents = [
     image: CloudStudyJam,
     title: "Cloud Kickoff: Pre-Study Jam Warm-Up",
     description:
-      "Join us on **23rd September at 8:00 PM** for a virtual workshop on Google Cloud. We'll introduce the upcoming **Cloud Study Jam (Oct 1-31)**, share tips to set up your Google Cloud profile and earn skill badges, discuss strategies to complete labs effectively, and answer your questions live. Whether you\'re a beginner or already exploring the cloud, this session is the perfect warm-up before the official jam begins.",
+      "Join us on **23rd September at 8:00 PM** for a virtual workshop on Google Cloud. We'll introduce the upcoming **Cloud Study Jam (Oct 1-31)**, share tips to set up your Google Cloud profile and earn skill badges, discuss strategies to complete labs effectively, and answer your questions live. Whether you're a beginner or already exploring the cloud, this session is the perfect warm-up before the official jam begins.",
     date: "2025-09-23",
     link: "https://gdg.community.dev/events/details/google-gdg-on-campus-government-college-of-engineering-and-ceramic-technology-kolkata-india-presents-cloud-kickoff-pre-study-jam-warm-up/",
   },
@@ -28,9 +29,17 @@ const allEvents = [
     image: Hacktoberfest,
     title: "Hacktoberfest Kickstart 2025: Fork, Push & Repeat",
     description:
-      "Hacktoberfest 2025 is here, and we\'re bringing the open-source spirit to GCECT! Join us for a session hosted by Google Developer Groups on Campus - GCECT, where we\'ll introduce Hacktoberfest 2025, explain how to participate, and guide you in making your first open-source contribution. We\'ll also review the progress of our Cloud Study Jam participants, highlight key milestones, share troubleshooting tips, and help beginners understand Git, GitHub, and open collaboration. Active contributors may even receive special recognition. Whether you\'re refining your open-source skills or exploring Google Cloud, this meetup is the perfect place to learn, share, and grow with the community.",
+      "Hacktoberfest 2025 is here, and we're bringing the open-source spirit to GCECT! Join us for a session hosted by Google Developer Groups on Campus - GCECT, where we'll introduce Hacktoberfest 2025, explain how to participate, and guide you in making your first open-source contribution. We'll also review the progress of our Cloud Study Jam participants, highlight key milestones, share troubleshooting tips, and help beginners understand Git, GitHub, and open collaboration. Active contributors may even receive special recognition. Whether you're refining your open-source skills or exploring Google Cloud, this meetup is the perfect place to learn, share, and grow with the community.",
     date: "2025-10-18",
     link: "https://gdg.community.dev/events/details/google-gdg-on-campus-government-college-of-engineering-and-ceramic-technology-kolkata-india-presents-hacktoberfest-kickstart-2025-fork-push-amp-repeat/",
+  },
+  {
+    id: 4,
+    image: GeminiHackDay,
+    title: "Gemini HackDays 2025 GCECT",
+    description: "A 5-hour celebration of innovation powered by **MLH and Google Gemini!** Join the brightest minds at GCECT to brainstorm, build prototypes, and showcase tech solutions. Whether you're creating web apps, data tools, or innovative designs, this hack day is your chance to learn, experiment, and collaborate with like-minded innovators in a fast-paced, friendly environment.",
+    date: "2025-11-03",
+    link: "https://forms.gle/SfAdwwijWiaJSebe8 ",
   },
 ];
 
